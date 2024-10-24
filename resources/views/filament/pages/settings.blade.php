4f2f80728c09e4d057798c3bfb3d634cdd732153
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 <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>
2024-10-24 12:55:33 -0300

    verificando

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,26 +1,200 @@
 <x-filament-panels::page>
 
-<main class="px-4 sm:px-6 lg:px-auto lg:px-8 lg:py-20">
+    <main class="px-4 sm:px-6 lg:px-auto lg:px-8 lg:py-20">
 
-    <h1 class="text-xl font-semibold leading-7 text-gray-900"> Dados Gerais</h1>
-
-
-    <div class="borde-t pt-6 mt-4"></div>
-
-    <div class="max-auto max-w-2xl space-y-8 lg:mx-0 lg:max-w-none">
-        <div>
-            
-            
-            {{ $this->form }}
+        <h1 class="text-xl font-semibold leading-7 text-gray-900"> Dados Gerais</h1>
 
 
-            <dl class="mt-6 space-y-6 border-t border-gray-200 text-sm leading-6">
-                
-            </dl>
+        <div class="pt-6 mt-4"></div>
+
+        <div class="max-auto max-w-2xl space-y-8 lg:mx-0 lg:max-w-none">
+            <div>
+
+
+                {{ $this->form }}
+
+
+                <dl class="mt-6 space-y-6 border-gray-200 text-sm leading-6"></dl>
+
+               
+                    
+            </div>
+            <div class="flex mx-auto justify-center items-center">
+                <div class="grid grid-cols-1 xl:grid-cols-3 gap-8 m-1">
+                    <div class="flex flex-col  w-full xl:w-96 bg-[#ffffff] text-[#374151] shadow-xl">
+                        <div class="flex flex-col p-8 h-full">
+                            <div class="text-xl text-center font-bold pb-6">Basic</div>
+                            <div class=" text-center text-sm pb-12">Basic features. Get started completely for free.</div>
+                            <div class="flex flex-col gap-3  text-sm">
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div class="font-bold">Core Features</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Limited Storage</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Ticket Support</div>
+                                </div>
+                            </div>
+                            <div class="flex flex-grow"></div>
+                            <div class="flex pt-10">
+                                <button class="w-full bg-[#16a34a] text-[#ffffff] font-bold text-sm p-3 rounded-lg hover:bg-black active:scale-95 transition-transform transform">Get started - 100% Free</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col  w-full xl:w-96 bg-[#ffffff] text-[#374151] shadow-xl">
+                        <div class="flex flex-col p-8 h-full">
+                            <div class="text-xl text-center font-bold pb-6">Pro</div>
+                            <div class=" text-center text-sm pb-12">Get access to advanced features for increased productivity.</div>
+                            <div class="flex flex-col gap-3  text-sm">
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div class="font-bold">All features of the basic plan</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Increased Storage</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Advanced Analytics</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Reporting Tools</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Third-Party Integrations</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>E-Mail Support</div>
+                                </div>
+                            </div>
+                            <div class="flex flex-grow"></div>
+                            <div class="flex pt-10">
+                                <button class="w-full bg-[#16a34a] text-[#ffffff] font-bold text-sm p-3 rounded-lg hover:bg-black active:scale-95 transition-transform transform">Buy Pro</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col  w-full xl:w-96 bg-[#ffffff] text-[#374151] shadow-xl">
+                        <div class="flex flex-col p-8 h-full">
+                            <div class="text-xl text-center font-bold pb-6">Premium</div>
+                            <div class=" text-center text-sm pb-12">Exclusive features and priority support for businesses.</div>
+                            <div class="flex flex-col gap-3  text-sm">
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div class="font-bold">All features of the Pro plan</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Unlimited Storage</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>End-to-End Encryption</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Predictive Insights</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Early-Access</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>Dedicated Account Manager</div>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <div class="text-green-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check">
+                                            <path d="M20 6 9 17l-5-5"></path>
+                                        </svg>
+                                    </div>
+                                    <div>24/7 Dedicated Customer Support</div>
+                                </div>
+                            </div>
+                            <div class="flex flex-grow"></div>
+                            <div class="flex pt-10">
+                                <button class="w-full bg-[#16a34a] text-[#ffffff] font-bold text-sm p-3 rounded-lg hover:bg-black active:scale-95 transition-transform transform">Buy Premium</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+           
+
 
         </div>
-    </div>
-</main>
+    </main>
 
 
 </x-filament-panels::page>
